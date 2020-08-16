1dce304b4a9c8296c39e4d88e5e51b5dcea48fe9
wim-web <rootuser.main+github@gmail.com>
2020-08-16 20:15:09 +0900

    Add atoms/Avatar

---
+++ src/atoms/Avatar.vue
@@ -0,0 +1,16 @@
+<template>
+    <img src="../assets/icon.jpg" alt="wim-icon" class="round">
+</template>
+
+<script>
+  export default {
+    name: "Avatar",
+  }
+</script>
+
+<style scoped lang="scss">
+    .round {
+        border-radius: 50%;
+        border: 3px black solid;
+    }
+</style>
