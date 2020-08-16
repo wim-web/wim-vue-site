641dd5809bb5a240448433b6fc987fd2b30670b6
wim-web <rootuser.main+github@gmail.com>
2020-08-16 20:36:23 +0900

    Add components/Profile

---
+++ src/components/Profile.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="profile">
+        <Avatar class="avatar-size" />
+        <h2>うぃむ</h2>
+        <p>そこら辺のWebエンジニア</p>
+    </div>
+</template>
+
+<script>
+  import Avatar from "../atoms/Avatar";
+
+  export default {
+    name: "Profile",
+    components: {Avatar}
+  }
+</script>
+
+<style scoped lang="scss">
+    
+    .profile {
+        text-align: center;
+    }
+    
+    .avatar-size {
+        height: 200px;
+        width: 200px;
+    }
+</style>
