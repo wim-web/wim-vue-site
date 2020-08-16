99a4f7236dffa43c85e5556fe4836c6ff23aaf7d
wim-web <rootuser.main+github@gmail.com>
2020-08-17 05:09:57 +0900

    Add components/MenuBar

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,59 @@
+<template>
+    <ul class="menu-bar">
+        <li v-for="menu in menuList" class="menu-bar__item">
+            <MenuItem
+                    :icon-data="menu.iconData"
+                    :text="menu.text"
+                    :handle-click="() => handleClickMenu(menu.text, menu.path)"
+                    :is-active="menu.isActive"
+                    :key="menu.text"
+            />
+        </li>
+    </ul>
+</template>
+
+<script>
+  import MenuItem from "../atoms/MenuItem";
+  
+  export default {
+    name: "MenuBar",
+    components: {MenuItem},
+    data: () => {
+      return {
+        menuList: [
+          {text: 'Skill', isActive: true, iconData: ['fas', 'bolt'], path: '/'},
+          {text: 'About', isActive: false, iconData: ['far', 'address-card'], path: '/about'},
+          {text: 'Link', isActive: false, iconData: ['fas', 'link'], path: 'link'},
+        ],
+      }
+    },
+    methods: {
+      handleClickMenu(text, path) {
+        this.menuList = this.menuList.map(menu => {
+          const isActive = menu.text === text;
+          return {
+            ...menu,
+            isActive
+          }
+        });
+        
+        this.$router.push(path);
+      },
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+    .menu-bar {
+        display: flex;
+        
+        &__item {
+            width: 100px;
+            margin-right: 20px;
+            
+            &:last-child {
+                margin-right: 0;
+            }
+        }
+    }
+</style>
