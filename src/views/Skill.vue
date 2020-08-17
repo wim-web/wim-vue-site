b35581c28e1d5578e78b904f489e3934cd12a741
wim-web <rootuser.main+github@gmail.com>
2020-08-17 18:24:52 +0900

    Add SkillPageの作成

--- src/views/Skill.vue
+++ src/views/Skill.vue
@@ -1,13 +1,26 @@
 <template>
-    <div>skill</div>
+    <div class="skill">
+        <skill-level-bar class="skill__bar"/>
+        <skill-list />
+    </div>
 </template>
 
 <script>
+  import SkillList from "../components/SkillList";
+  import SkillLevelBar from "../components/SkillLevelBar";
+
   export default {
-    name: "Skill"
+    name: "Skill",
+    components: {SkillLevelBar, SkillList},
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .skill {
+        text-align: center;
+        
+        &__bar {
+            margin-bottom: 40px;
+        }
+    }
 </style>
