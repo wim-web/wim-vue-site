26913ab03674c259e4a878e6c69ab547fab9199d
wim-web <rootuser.main+github@gmail.com>
2020-08-17 13:36:38 +0900

    Add some routes Skill, About, Link

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Skill from "../views/Skill";
+
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: '/',
+    name: 'skill',
+    component: Skill,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import(`@/views/About.vue`),
+  },
+  {
+    path: '/link',
+    name: 'link',
+    component: () => import(`@/views/Link.vue`),
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
--- src/views/Link.vue
+++ src/views/Link.vue
@@ -1,5 +1,5 @@
 <template>
-    
+    <div>link</div>
 </template>
 
 <script>
