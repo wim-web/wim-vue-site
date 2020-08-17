db25f72d7c354feff8ae9f07d1989dbdbd17b281
wim-web <rootuser.main+github@gmail.com>
2020-08-17 17:53:49 +0900

    Add components/SkillLevelBar

---
+++ src/components/SkillLevelBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="skill-level-bar">
+        <i class="skill-level-bar__item" v-for="data in iconData">
+            <font-awesome-icon :icon="data" size="lg" />
+        </i>
+    </div>
+</template>
+
+<script>
+
+  import iconData from '../_js/skill-level-icon';
+
+  export default {
+    name: "SkillLevelBar",
+    data: () => {
+      return {
+        iconData
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+    .skill-level-bar {
+        background-color: $primary;
+        color: whitesmoke;
+        padding: 10px 15px;
+        display: inline-block;
+        border-radius: 8px;
+        
+        &__item {
+            
+            &:last-child {
+                &::after {
+                    content: '';
+                    margin: 0;
+                }
+            }
+            
+            &::after {
+                content: '<';
+                margin: 0 8px;
+            }
+        }
+    }
+</style>
