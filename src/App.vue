6a05dbd2d121d6a5965ddee4a447964b8b93d7b0
wim-web <rootuser.main+github@gmail.com>
2020-08-17 18:30:01 +0900

    Add Main画面の作成

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,43 @@
 <template>
-  <div id="app">
-  
-  </div>
+    <div id="app">
+        <main class="main">
+            <profile class="profile"/>
+            <menu-bar class="menu-bar"/>
+            <transition mode="out-in">
+                <router-view/>
+            </transition>
+        </main>
+    </div>
 </template>
+
+<script>
+  import Profile from "./components/Profile";
+  import MenuBar from "./components/MenuBar";
+
+  export default {
+    components: {MenuBar, Profile},
+  }
+</script>
+
+<style scoped lang="scss">
+    .main {
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+    
+    .profile {
+        margin-top: 50px;
+    }
+    
+    .menu-bar {
+        margin: 40px 0;
+    }
+    
+    .v-enter-active, .v-leave-active {
+        transition: .3s;
+    }
+    
+    .v-enter, .v-leave-to {
+        opacity: 0;
+    }
+</style>
