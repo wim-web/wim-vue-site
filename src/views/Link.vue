fc4e941ecb09e21c96973c86d3b40ea0fbd78979
wim-web <rootuser.main+github@gmail.com>
2020-08-17 19:37:27 +0900

    Add Link Page

--- src/views/Link.vue
+++ src/views/Link.vue
@@ -1,13 +1,44 @@
 <template>
-    <div>link</div>
+    <div class="link-button-list">
+        <link-button
+                class="link-button-list__item"
+                v-for="link in links"
+                :text="link.text"
+                :icon-data="link.iconData"
+                :link="link.link"
+        />
+    </div>
 </template>
 
 <script>
+  import LinkButton from "../atoms/LinkButton";
+
   export default {
-    name: "Link"
+    name: "Link",
+    components: {LinkButton},
+    data: () => {
+      return {
+        links: [
+          {text: 'Twitter', iconData: ['fab', 'twitter'], link: 'https://twitter.com/wim_web'},
+          {text: 'Github', iconData: ['fab', 'github'], link: 'https://github.com/wim-web'},
+          {text: 'Qiita', iconData: ['fas', 'link'], link: 'https://qiita.com/wim'},
+        ],
+      }
+    }
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .link-button-list {
+        display: flex;
+        justify-content: center;
+        
+        &__item {
+            margin-right: 20px;
+            
+            &:last-child {
+                margin-right: 0;
+            }
+        }
+    }
 </style>
